74f0920deab6261505e13e2d5b8eec7dc750baff
JStrummer <64981369+JStrummer@users.noreply.github.com>
2021-11-08 21:25:13 +0100

    separated graphic logic

--- src/home.js
+++ src/home.js
@@ -12,30 +12,49 @@ function initHome() {
   arrayFromRange(1, 25).forEach((day, index) => {
     let canvas = document.createElement("canvas");
     canvas.classList.add("calendar-box");
-    if (today < new Date(2021, 10, day)) {
-      canvas.classList.add("blocked");
-    }
+
     canvas.dataset.day = day;
     canvas.width = canvas.height = root.offsetWidth / 5;
 
-    let ctx = canvas.getContext("2d");
-
-    ctx.drawImage(
-      img,
-      (index % 5) * sourceStep,
-      Math.floor(index / 5) * sourceStep,
-      sourceStep,
-      sourceStep,
-      0,
-      0,
-      canvas.width,
-      canvas.height
-    );
-
-    if (canvas.classList.contains("blocked")) {
-      ctx.fillStyle = "rgba(0,0,0,0.8)";
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    }
     root.appendChild(canvas);
   });
+
+  blokFutureDate();
+  drawBoxes();
+
+  function blokFutureDate() {
+    let boxes = document.querySelectorAll(".calendar-box");
+
+    boxes.forEach(box => {
+      let { day } = box.dataset;
+      if (today < new Date(2021, 10, day)) {
+        box.classList.add("blocked");
+      }
+    });
+  }
+
+  function drawBoxes() {
+    let boxes = document.querySelectorAll(".calendar-box");
+
+    boxes.forEach((box, index) => {
+      let ctx = box.getContext("2d");
+
+      ctx.drawImage(
+        img,
+        (index % 5) * sourceStep,
+        Math.floor(index / 5) * sourceStep,
+        sourceStep,
+        sourceStep,
+        0,
+        0,
+        box.width,
+        box.height
+      );
+
+      if (box.classList.contains("blocked")) {
+        ctx.fillStyle = "rgba(0,0,0,0.8)";
+        ctx.fillRect(0, 0, box.width, box.height);
+      }
+    });
+  }
 }
