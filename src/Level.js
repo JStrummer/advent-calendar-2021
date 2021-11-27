ef5693217f54a0ebf2c6c87271c46c105634136c
JStrummer <64981369+JStrummer@users.noreply.github.com>
2021-11-27 11:36:06 +0100

    added mouse controls

--- src/Level.js
+++ src/Level.js
@@ -6,6 +6,7 @@ class Level {
     this.tilesNumber = 9;
     this.tiles = new Grid(this.tilesNumber);
     this.draw = new Draw(this.number);
+    this.startMove = null;
 
     this.init();
   }
@@ -14,7 +15,10 @@ class Level {
     this.tiles.shuffle();
     this.draw.init(this.tiles.tiles);
 
-    document.addEventListener("keydown", this.handleClick.bind(this));
+    document.addEventListener("keydown", this.handleKeyPress.bind(this));
+    // movement must start inside the canvas
+    this.draw.canvas.addEventListener("mousedown", this.moveStart.bind(this));
+    document.addEventListener("mouseup", this.moveEnd.bind(this));
   }
 
   get won() {
@@ -30,16 +34,63 @@ class Level {
     return result;
   }
 
-  handleClick(ev) {
-    if (/^Arrow/.test(ev.key)) {
-      let direction = ev.key.match(/^Arrow([a-zA-Z]+)/)[1].toLowerCase();
+  checkWin() {
+    if (this.won) {
+      console.log("WINNER");
+      this.draw.solution();
+    }
+  }
+
+  moveStart(event) {
+    this.startMove = getCoord(event);
+  }
+
+  moveEnd(event) {
+    if (not(isNull)(this.startMove)) {
+      let endMove = getCoord(event);
+
+      let movements = {
+        x: endMove.x - this.startMove.x,
+        y: endMove.y - this.startMove.y
+      };
+
+      let movement = greaterAbs(movements);
+
+      if (this.tiles.move(getDirection(movement))) {
+        this.draw.grid(this.tiles.tiles);
+
+        this.checkWin();
+      }
+    }
+
+    this.startMove = null;
+
+    function greaterAbs({ x, y }) {
+      return Math.abs(x) > Math.abs(y) ? { x } : { y };
+    }
+
+    function getDirection(movement) {
+      let result;
+
+      let [axis, value] = Object.entries(movement)[0];
+
+      if (axis === "x") {
+        result = value > 0 ? "right" : "left";
+      } else if (axis === "y") {
+        result = value > 0 ? "down" : "up";
+      }
+
+      return result;
+    }
+  }
+
+  handleKeyPress(event) {
+    if (/^Arrow/.test(event.key)) {
+      let direction = event.key.match(/^Arrow([a-zA-Z]+)/)[1].toLowerCase();
       if (this.tiles.move(direction)) {
         this.draw.grid(this.tiles.tiles);
 
-        if (this.won) {
-          console.log("WINNER");
-          this.draw.solution();
-        }
+        this.checkWin();
       }
     }
   }
